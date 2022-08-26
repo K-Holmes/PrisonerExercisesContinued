04722c864a25a9a29d4b1f68a57820ebcd422876
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-26 12:07:49 -0400

    "Finished Activities"

---
+++ src/PrisonTest_Student_7_3.java
@@ -0,0 +1,11 @@
+//Section 7, Lesson 3 Starter for Exercise 1 - Slide 6
+
+public class PrisonTest_Student_7_3 {
+
+    public static void main(String[] args) {
+        Prisoner_Student_7_3 bubba = new Prisoner_Student_7_3("Bubba", 2.08, 3);
+        Prisoner_Student_7_3 twitch = new Prisoner_Student_7_3("Twitch", 1.73, 4);
+
+
+    }
+}
---
+++ src/Prisoner_Student_7_3.java
@@ -0,0 +1,23 @@
+//Section 7, Lesson 3 Starter for Exercise 1 - Slide 6
+
+public class Prisoner_Student_7_3 {
+
+    //Fields
+    public String name;
+    public double height;
+    public int sentence;
+
+    //Constructor
+    Prisoner_Student_7_3(String name, double height, int sentence) {
+        this.sentence=sentence;
+        this.name=name;
+        this.height=height;
+    }
+
+
+
+    //Methods
+    public void think(){
+        System.out.println("I'll have my revenge.");
+    }
+}
